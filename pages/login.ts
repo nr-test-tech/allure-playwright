578295dd1a3f211b980efb82bca156721e702366
nisharani <nisharani@digitalnirvana.com>
2025-05-10 17:48:36 +0530

    playwright e2e

---
+++ pages/login.ts
@@ -0,0 +1,31 @@
+import { Page, Locator } from '@playwright/test';
+
+export class LoginPage {
+  readonly page: Page;
+  readonly username: Locator;
+  readonly password: Locator;
+  readonly loginButton: Locator;
+  readonly errorMessage : Locator;;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.username = page.locator('#user-name'); 
+    this.password = page.locator('#password');
+    this.loginButton = page.locator('#login-button');
+    this.errorMessage =  page.locator("h3[data-test='error']");
+  }
+
+  async login(user: string, pass: string) {
+    await this.username.fill(user);
+    await this.password.fill(pass);
+    await this.loginButton.click();
+  }
+
+  async getErrormsg()
+  {
+    const errMessage =await this.errorMessage.textContent();
+    console.log('error---'+errMessage);
+    return errMessage;
+ }
+ 
+}
---
+++ tests/login.spec.ts
@@ -0,0 +1,47 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/login';
+import * as users from '../data/test-data.json';
+
+
+test.describe('Login', () => {
+    let loginPage: LoginPage;
+
+    test.beforeEach(async ({ page, baseURL }) => {
+        loginPage = new LoginPage(page);
+        await page.goto(`${baseURL}`);
+    })
+
+    test('verify login with valid credentials', async ({ page }) => {
+        await loginPage.login(users.admin.username, users.admin.password);
+        // Assert that the URL changed to the inventory page (example of a successful login)
+        await expect(page).toHaveURL(`/inventory.html`);
+        // Assert that the inventory list is visible (indicating a successful login)
+        const inventoryList = await page.locator('.inventory_list');
+        await expect(inventoryList).toBeVisible();
+        // Optionally, check for a welcome message or other indicator
+        const welcomeMessage = await page.locator('.title'); // Adjust selector as needed
+        await expect(welcomeMessage).toBeVisible();
+        await expect(welcomeMessage).toHaveText('Products')
+    });
+
+
+    // Test: Verify login with invalid credentials
+    test('Verify login with invalid credentials', async ({ page }) => {
+        await loginPage.login(users.customer.username, users.customer.password);
+        // Assert that the error message is visible
+        const errorMessage = await loginPage.getErrormsg();
+        await expect(errorMessage).toBe('Epic sadface: Sorry, this user has been locked out.');
+    });
+
+
+    test("Negative: User tries to login without credentials.", async ({ page }) => {
+        await loginPage.login("", "");
+        // Assert that the error message is visible
+        const errorMessage = await loginPage.getErrormsg();
+        await expect(errorMessage).toBe('Epic sadface: Username is required');
+
+    });
+
+});
+
+
